60e18b1994fa7fa9b50125d86f7d39f0e9fbf7ef
AhmadKhatttab <ahmad.amer.black@gmail.com>
2026-05-20 12:33:13 +0300

    احمد خطاب
    حطيت كومنتات توضيحية لملف database.py

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
+#هذا الكود هو المحرك الاساسي الي رح يربط الداتابيس بالاكواد الي احنا عملناها
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
+#هذا الرابط الي هون هو الي مخلي مشروعنا سكيل ابيليتي و قابل للتوسع عشان لما لقدام
 DATABASE_URL = "sqlite:///opensooq_data.db"
 
+#هون انشاء المحرك و الجلسة الي رح تكون توخذ و تعطي بين الكود و لداتابيس
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
+#هون الأب او بنقدر نحكي الاساس الي عمل قاعدة البيانات 
 Base = declarative_base()
 Base.query = db_session.query_property()
 
